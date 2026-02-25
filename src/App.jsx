5804f5aeb3141f99a5d534554e3d7aec66200e76
Tomikimi <tomikimiogoli@yahoo.com>
2026-02-25 11:50:15 +0100

    creation of Friends Design

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+function App() {
+  return (
+    <>
+      <div>
+        <h2>Hi, Tomikimi</h2>
+      </div>
+      <div className="app">
+        <div>
+          <div className="sidebar">
+            <ul>
+              <li>
+                <img src="https://i.pravatar.cc/35" alt="" />
+                <h3>Daniel</h3>
+                <p>You and Daniel are even</p>
+                <button type="button" className="button">
+                  Select
+                </button>
+              </li>
+              <li>
+                <img src="https://i.pravatar.cc/40" alt="" />
+                <h3>Fisoye</h3>
+                <p>You and Fisoye are even</p>
+                <button type="button" className="button">
+                  Select
+                </button>
+              </li>
+              <li>
+                <img src="https://i.pravatar.cc/31" alt="" />
+                <h3>Otagbo</h3>
+                <p>You and Otagbo are even</p>
+                <button type="button" className="button">
+                  Select
+                </button>
+              </li>
+            </ul>
+            <div className="button">Add Friend</div>
+          </div>
+        </div>
+        <div>bill form</div>
+      </div>
+    </>
+  );
+}
+
+export default App;
